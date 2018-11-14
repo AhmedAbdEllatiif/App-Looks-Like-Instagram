336d4908544685ce4962436de4d1a41b0733753a
Ahmed <ahmedmohamedaneng@gmail.com>
2018-11-14 10:36:36 +0200

    WIFIBroadCastReceiver_added

---
+++ app/src/main/java/com/example/ahmedd/firabasetest/Receivers/WIFIBroadCastReceiver.java
@@ -0,0 +1,15 @@
+package com.example.ahmedd.firabasetest.Receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class WIFIBroadCastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+
+    
+}
