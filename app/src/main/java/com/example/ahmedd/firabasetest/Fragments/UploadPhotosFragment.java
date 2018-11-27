50343b3a900612b629d1854ef7dd576d8c7df1b2
Ahmed <ahmedmohamedaneng@gmail.com>
2018-11-27 15:00:02 +0200

    toolbar photoAcrivity_added

--- app/src/main/java/com/example/ahmedd/firabasetest/Fragments/UploadPhotosFragment.java
+++ app/src/main/java/com/example/ahmedd/firabasetest/Fragments/UploadPhotosFragment.java
@@ -1,12 +1,20 @@
 package com.example.ahmedd.firabasetest.Fragments;
 
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.ahmedd.firabasetest.PhotoActivity;
 import com.example.ahmedd.firabasetest.R;
 
 /**
@@ -14,6 +22,12 @@ import com.example.ahmedd.firabasetest.R;
  */
 public class UploadPhotosFragment extends Fragment {
 
+    private View view;
+    private Button btn_pick_photo;
+    private TextInputEditText edit_txt_photo_name;
+    private ImageView img_upload;
+    private Button btn_upload;
+    private TextView txt_showUploads;
 
     public UploadPhotosFragment() {
         // Required empty public constructor
@@ -24,7 +38,28 @@ public class UploadPhotosFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_upload_photos, container, false);
+       view = inflater.inflate(R.layout.fragment_upload_photos, container, false);
+
+        btn_pick_photo = view.findViewById(R.id.btn_pick_photo);
+        edit_txt_photo_name = view.findViewById(R.id.edit_txt_photo_name);
+        img_upload = view.findViewById(R.id.img_upload);
+        btn_upload = view.findViewById(R.id.btn_upload);
+        txt_showUploads = view.findViewById(R.id.txt_showUploads);
+
+
+        MyClickListeners();
+
+        return view;
+    }
+
+    private void MyClickListeners(){
+        txt_showUploads.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =  new Intent(getActivity(), PhotoActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
--- app/src/main/java/com/example/ahmedd/firabasetest/PhotoActivity.java
+++ app/src/main/java/com/example/ahmedd/firabasetest/PhotoActivity.java
@@ -1,13 +1,37 @@
 package com.example.ahmedd.firabasetest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class PhotoActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
+
+        setToolBar();
+    }
+
+    private void setToolBar() {
+
+        //toolbar = findViewById(R.id.photoActivity_ToolBar);
+        Toolbar toolbar = findViewById(R.id.photoActivity_ToolBar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PhotoActivity.this,MainActivity.class)
+                        .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            }
+        });
     }
 }
