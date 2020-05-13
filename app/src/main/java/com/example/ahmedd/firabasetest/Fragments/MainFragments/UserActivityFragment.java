42598334921ea8cfba59f0e820049b5375d393ed
AhmedHome <ahmedmohamedaneng@gmail.com>
2020-05-13 13:03:46 +0200

    toolbar of FragmentActivityUser added

--- app/src/main/java/com/example/ahmedd/firabasetest/Fragments/MainFragments/UserActivityFragment.java
+++ app/src/main/java/com/example/ahmedd/firabasetest/Fragments/MainFragments/UserActivityFragment.java
@@ -24,6 +24,6 @@ public class UserActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_uset_activiy, container, false);
+        return inflater.inflate(R.layout.fragment_user_activiy, container, false);
     }
 }
